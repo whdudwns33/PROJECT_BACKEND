9b64f4485b53250c1850cbb1e236baa14663f774
jjun11 <jok6961@gmail.com>
2023-12-06 17:17:23 +0900

    2023-12-06 장현준

--- src/main/java/com/projectBackend/project/controller/PerformanceController.java
+++ src/main/java/com/projectBackend/project/controller/PerformanceController.java
@@ -5,10 +5,9 @@ import com.projectBackend.project.dto.PerformanceDto;
 import com.projectBackend.project.service.PerformanceService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,36 @@ public class PerformanceController {
         return ResponseEntity.ok(list);
     }
 
+    // 공연 등록
+    @PostMapping("/new")
+    public ResponseEntity<Boolean> performanceRegister(@RequestBody PerformanceDto performanceDto) {
+        boolean isTrue = performanceService.savePerformance(performanceDto);
+        return ResponseEntity.ok(isTrue);
+    }
+
+
+    // 공연 삭제
+    @DeleteMapping("/delete")
+    public ResponseEntity<Boolean> performanceDelete() {
+        performanceService.deleteAll();
+        return ResponseEntity.ok(true);
+    }
+
+    // 페이지네이션
+    @GetMapping("/list/page")
+    public ResponseEntity<List<PerformanceDto>> performanceList(@RequestParam(defaultValue = "0") int page,
+                                                                @RequestParam(defaultValue = "10") int size) {
+        List<PerformanceDto> list = performanceService.getPerformanceList(page, size);
+        log.info("list : {}", list);
+        return ResponseEntity.ok(list);
+    }
+    // 페이지 수 조회
+    @GetMapping("/list/count")
+    public ResponseEntity<Integer> performancePage(@RequestParam(defaultValue = "0") int page,
+                                                   @RequestParam(defaultValue = "10") int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        int count =  performanceService.getPerformancePage(pageRequest);
+        return ResponseEntity.ok(count);
+    }
 
 }
--- src/main/java/com/projectBackend/project/service/PerformanceService.java
+++ src/main/java/com/projectBackend/project/service/PerformanceService.java
@@ -6,6 +6,8 @@ import com.projectBackend.project.entity.Performance;
 import com.projectBackend.project.repository.PerformanceRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,6 +19,7 @@ import java.util.List;
 public class PerformanceService {
     private final PerformanceRepository performanceRepository;
 
+    // 공연 조회
     public List<PerformanceDto> getPerformanceList() {
         List<Performance> performances = performanceRepository.findAll();
         List<PerformanceDto> performanceDtos = new ArrayList<>();
@@ -25,6 +28,55 @@ public class PerformanceService {
         }
         return performanceDtos;
 
+    }
+
+    // 공연 등록
+    public boolean savePerformance(PerformanceDto performanceDto) {
+        try {
+            Performance performance = new Performance();
+            // 멤버 및 카테고리 검증 필요
+//            Member member = memberRepository.findByEmail(boardDto.getEmail()).orElseThrow(
+//                    () -> new RuntimeException("해당 회원이 존재하지 않습니다.")
+//            );
+//            Category category = categoryRepository.findById(boardDto.getCategoryId()).orElseThrow(
+//                    () -> new RuntimeException("해당 카테고리가 존재하지 않습니다.")
+//            );
+            performance.setPerformanceName(performanceDto.getPerformanceName());
+            performance.setPerformer(performanceDto.getPerformer());
+            performance.setVenue(performanceDto.getVenue());
+            performance.setDetailVenue(performanceDto.getDetailVenue());
+            performance.setPerformanceDate(performanceDto.getPerformanceDate());
+            performance.setPrice(performanceDto.getPrice());
+            performance.setDescription(performanceDto.getDescription());
+            performance.setSeatCount(performanceDto.getSeatCount());
+            performance.setPerformanceImage(performanceDto.getPerformanceImage());
+            performanceRepository.save(performance);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    // 공연 삭제
+    public void deleteAll() {
+        performanceRepository.deleteAll();
+    }
+
+    // 페이지네이션
+    public List<PerformanceDto> getPerformanceList(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        List<Performance> performances = performanceRepository.findAll(pageable).getContent();
+        List<PerformanceDto> performanceDtos = new ArrayList<>();
+        for (Performance performance : performances) {
+            performanceDtos.add(convertEntityToDto(performance));
+        }
+        return performanceDtos;
+    }
+
+    // 페이지 수 조회
+    public int getPerformancePage(Pageable pageable) {
+        return performanceRepository.findAll(pageable).getTotalPages();
     }
 
     // 퍼포먼스 엔티티를 DTO로 변환
