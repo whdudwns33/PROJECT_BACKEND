8d4a4cc75e61b73b3cddd704b33254982068aa55
JH <dafr47@naver.com>
2023-12-14 10:01:42 +0900

    길종환 커뮤니티 추가

---
+++ src/main/java/com/projectBackend/project/entity/CommunityView.java
@@ -0,0 +1,27 @@
+package com.projectBackend.project.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Table(name = "view")
+@NoArgsConstructor
+public class CommunityView {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+    @Column(name = "ip")
+    private String ip;
+}
