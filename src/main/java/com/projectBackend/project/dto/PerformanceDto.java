67cafd6e0be8ba45d48d38766f21dea7bba869a5
jjun11 <jok6961@gmail.com>
2023-12-15 19:30:09 +0900

    2023-12-15 장현준

--- src/main/java/com/projectBackend/project/dto/PerformanceDto.java
+++ src/main/java/com/projectBackend/project/dto/PerformanceDto.java
@@ -3,12 +3,14 @@ package com.projectBackend.project.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class PerformanceDto {
     private Long performanceId;
     private String performanceName;
-    private String performer;
+    private List<String> performer;
     private String venue;
     private String detailVenue;
     private String performanceDate;
