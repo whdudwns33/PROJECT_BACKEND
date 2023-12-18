9a7254229f62e61a5c7ff7bf8d296d0ba66f354b
jyj051811@gmail.com <jyj051811@gmail.com>
2023-12-18 15:35:26 +0900

    카카오 로그인 수정 및 메인 페잊

--- src/main/java/com/projectBackend/project/service/AuthService.java
+++ src/main/java/com/projectBackend/project/service/AuthService.java
@@ -154,7 +154,6 @@ public class AuthService {
         }
     }
 
-
     // 카카오 로그인 이후 랜덤 패스워드 생성
     public String generateRandomPassword() {
         UUID uuid = UUID.randomUUID();
@@ -187,6 +186,8 @@ public class AuthService {
         log.warn("닉네임 중복 확인 {} : ", isTrue);
         return isTrue;
     }
+
+
     // 길종환
     public UserResDto getUserInfo(String token) {
         String email = tokenProvider.getUserEmail(token);
--- src/main/java/com/projectBackend/project/service/KakaoService.java
+++ src/main/java/com/projectBackend/project/service/KakaoService.java
@@ -23,8 +23,6 @@ import static com.projectBackend.project.utils.Common.*;
 @RequiredArgsConstructor
 @Transactional
 public class KakaoService {
-//    private final TokenRepository tokenRepository;
-//    private final TokenDto tokenDto;
 
     // 카카오 로그인 토큰 발급
     public String kakaoToken (String code) {
@@ -58,21 +56,12 @@ public class KakaoService {
             log.info("response {} : ", response.getBody());
             System.out.println("status" + response.getStatusCodeValue());
             if (response.getStatusCodeValue() == 200) {
-                // 카카오 토큰 정보를 저장하기 위한 객체 생성
-//                Token token = new Token();
                 ObjectMapper objectMapper = new ObjectMapper();
-
                 // JSON 문자열을 JsonNode로 변환 및 토큰 정보 출력
                 JsonNode jsonNode = objectMapper.readTree(response.getBody());
                 String accessToken = jsonNode.get("access_token").asText();
-//                String tokenType = jsonNode.get("token_type").asText();
-//                String refreshToken = jsonNode.get("refresh_token").asText();
-//                Long accessExpire = Long.valueOf(jsonNode.get("expires_in").asText());
-//                Long refreshExpire = Long.valueOf(jsonNode.get("refresh_token_expires_in").asText());
-
                 log.info("Access Token: {}", accessToken);
-//                log.info("Token Type: {}", tokenType);
-//                log.info("Refresh Token: {}", refreshToken);
+                // 발급 받은 엑세스 토큰으로 이메일 데이터 출력
                 String kakaoEmailData = kakaoEmail(accessToken);
                 log.info("kakao email : {}", kakaoEmailData);
                 return kakaoEmailData;
@@ -90,23 +79,17 @@ public class KakaoService {
 
 
     // 카카오 이메일 받아오기
-    public String kakaoEmail (String accessToken) {
+    public String kakaoEmail(String accessToken) {
         try {
             RestTemplate rt = new RestTemplate();
-            // 토큰 가져오기 => 수정 필요
             ObjectMapper objectMapper = new ObjectMapper();
-//        System.out.println("accessToken : " + accessToken);
 
-            // HttpHeader 오브젝트 생성
             HttpHeaders headers = new HttpHeaders();
             headers.set("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
             headers.set("Authorization", "Bearer " + accessToken);
-//        System.out.println("headers : " + headers);
 
-            // get 방식은 body가 필요 없지만 exchange를 쓰기 위해서는 필요
             HttpEntity<String> requestEntity = new HttpEntity<>("request body content", headers);
 
-            // GET 요청하기 - response 변수의 응답받음.
             ResponseEntity<String> response = rt.exchange(
                     "https://kapi.kakao.com/v2/user/me",
                     HttpMethod.GET,
@@ -114,29 +97,29 @@ public class KakaoService {
                     String.class,
                     headers
             );
-            System.out.println("Get RESPONSE : " + response);
+
+            // 추가: 응답 내용 로깅
+            log.info("Get RESPONSE : {}", response);
+
             if (response.getStatusCodeValue() == 200) {
-                // json 문자열의 바디 부분을 객체화
-                // email의 경우 kakao_acount 내부에 존재
                 JsonNode jsonNode = objectMapper.readTree(response.getBody());
                 JsonNode kakaoAccountNode = jsonNode.path("kakao_account");
                 String email = kakaoAccountNode.path("email").asText();
                 if (email != null) {
-                    log.info("email123 {} :", email);
+                    log.info("Received email: {}", email);
                     return email;
-                }
-                else {
+                } else {
                     log.warn("카카오 이메일이 전송되지 않음");
                     return null;
                 }
-            }
-            else {
-                System.out.println("카카오 유저 연결이 되지 않음");
+            } else {
+                // 추가: 실패한 경우 응답 내용 로깅
+                log.error("카카오 유저 연결이 되지 않음. 응답: {}", response);
                 return null;
             }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            // 추가: 예외가 발생한 경우 스택 트레이스 출력
+            log.error("카카오 이메일 요청 중 예외 발생", e);
             return null;
         }
     }
