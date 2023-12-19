f6482055c2c8e0562fbb12796fd32e44c1779061
jjun11 <jok6961@gmail.com>
2023-12-19 17:33:40 +0900

    2023-12-18 장현준, 오타수정

--- src/main/java/com/projectBackend/project/controller/PerformerController.java
+++ src/main/java/com/projectBackend/project/controller/PerformerController.java
@@ -13,14 +13,16 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/perofrmer")
+@RequestMapping("/performer")
 @RequiredArgsConstructor
 public class PerformerController {
     private final PerformerService performerService;
+    //전체공연자조회
     @GetMapping("/list")
     public ResponseEntity<List<PerformerDto>> performerList() {
         System.out.println("컨트롤러 performerList");
         List<PerformerDto> list = performerService.getPerformerList();
+        System.out.println("컨트롤러 performerList : " + list);
         return ResponseEntity.ok(list);
     }
 
