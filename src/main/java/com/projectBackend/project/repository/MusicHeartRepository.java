baf5aed82a95dc1e27b85d7ab875e3bc9c0997df
jyj051811@gmail.com <jyj051811@gmail.com>
2023-12-22 19:42:24 +0900

    12.22 조영준

--- src/main/java/com/projectBackend/project/repository/MusicHeartRepository.java
+++ src/main/java/com/projectBackend/project/repository/MusicHeartRepository.java
@@ -4,19 +4,17 @@ import com.projectBackend.project.entity.MusicHeart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface MusicHeartRepository extends JpaRepository<MusicHeart, Long> {
     List<MusicHeart> findByMusic_MusicId(Long musicId);
-//    MusicHeart findByMusicIdAndUserId(Long musicId, Long userId);
-
 
     @Transactional
     @Modifying
     @Query("DELETE FROM MusicHeart mh WHERE mh.music.id = :musicId AND mh.member.id = :userId")
-//    MusicHeart  findByMusic_MusicIdAndMember_UserId(Long musicId, Long userId);
-    void deleteByMusic_MusicIdAndMember_UserId(Long musicId, Long userId);
+    void deleteByMusic_MusicIdAndMember_UserId(@Param("musicId")Long musicId, @Param("userId") Long userId);
 
 }
