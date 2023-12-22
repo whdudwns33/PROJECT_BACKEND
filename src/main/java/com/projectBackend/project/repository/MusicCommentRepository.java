5f5d1bb570619ed9c2a6d45994dc997da36fb5b1
Croppedeyebrow <lee940706@gmail.com>
2023-12-22 19:31:29 +0900

    12월 22일 댓글 기능 추가.

--- src/main/java/com/projectBackend/project/repository/MusicCommentRepository.java
+++ src/main/java/com/projectBackend/project/repository/MusicCommentRepository.java
@@ -1,12 +1,15 @@
-//package com.projectBackend.project.repository;
-//
-//import com.projectBackend.project.entity.MusicComment;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import java.util.List;
-//
-//public interface MusicCommentRepository extends JpaRepository<MusicComment, Long> {
-//    List<MusicComment> findByCommentContentContaining(String keyword);
-//
-//    List<MusicComment>findByMusicId(Long musicId);
-//}
+package com.projectBackend.project.repository;
+
+import com.projectBackend.project.entity.MusicComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.util.List;
+public interface MusicCommentRepository extends JpaRepository<MusicComment, Long> {
+    List<MusicComment> findByContentContaining(String keyword);
+    List<MusicComment> findByMusic(Long musicId);
+
+    List<MusicComment> findByMusic_MusicId(Long musicId);
+    //findByMusic_MusicId' 메서드명으로 선언하여 Music ID를 기반으로 댓글을 조회
+
+}
