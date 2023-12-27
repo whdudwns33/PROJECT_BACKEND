b590562910a89ecfe628f636d82cbd49e32f023b
jyj051811@gmail.com <jyj051811@gmail.com>
2023-12-28 08:49:37 +0900

    관리자 페이지

---
+++ src/main/java/com/projectBackend/project/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.projectBackend.project.controller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Setter
+@Getter
+@ToString
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminController {
+}
